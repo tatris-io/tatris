1aea1fc7d81d21116370bfb8750bbbdb3bcfff56
Arthur Chern <weirong.cwr@gmail.com>
2022-12-05 16:37:34 +0800

    feat: add lint, formatter and revive (#3)

--- cmd/meta/meta.go
+++ cmd/meta/meta.go
@@ -1,7 +1,8 @@
 // Copyright 2022 Tatris Project Authors. Licensed under Apache-2.0.
 
+// binary entry point for the meta service
 package main
 
 func main() {
-    println("init project for meta")
+	println("init project for meta")
 }
