f4b94702863be79988f93160536ece08592ce6d8
Arthur Chern <weirong.cwr@gmail.com>
2023-01-05 17:36:22 +0800

    chore: add standalone boot log

--- cmd/standalone/standalone.go
+++ cmd/standalone/standalone.go
@@ -4,17 +4,22 @@
 package main
 
 import (
+	"github.com/tatris-io/tatris/internal/common/log/logger"
 	"github.com/tatris-io/tatris/internal/ingestion"
 	"github.com/tatris-io/tatris/internal/meta"
 	"github.com/tatris-io/tatris/internal/query"
 	"github.com/tatris-io/tatris/internal/service"
+	"go.uber.org/zap"
 )
 
+const role string = "Standalone"
+
 func main() {
+	logger.Info("Tatris server is booting", zap.String("role", role))
+
 	ingestion.SayHello()
 	meta.SayHello()
 	query.SayHello()
-	service.SayHello()
 
 	service.StartHTTPServer("all")
 
