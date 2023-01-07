8f7247e0100a63439606d5a45afa94a08088c474
Arthur Chern <weirong.cwr@gmail.com>
2023-01-07 22:52:13 +0800

    feat: add some examples

--- internal/common/log/logger/global_logger.go
+++ internal/common/log/logger/global_logger.go
@@ -39,7 +39,7 @@ func Debug(msg string, fields ...zap.Field) {
 
 // Debugf wraps Debugf of sugared logger, it uses fmt.Sprintf to log a templated message.
 func Debugf(template string, args ...interface{}) {
-	_sugaredGlobalLogger.Debugf(template, args)
+	_sugaredGlobalLogger.Debugf(template, args...)
 }
 
 // Info logs a message at InfoLevel. The message includes any fields passed at the log site, as well
@@ -50,7 +50,7 @@ func Info(msg string, fields ...zap.Field) {
 
 // Infof wraps Infof of sugared logger, it uses fmt.Sprintf to log a templated message.
 func Infof(template string, args ...interface{}) {
-	_sugaredGlobalLogger.Infof(template, args)
+	_sugaredGlobalLogger.Infof(template, args...)
 }
 
 // Warn logs a message at WarnLevel. The message includes any fields passed at the log site, as well
@@ -61,7 +61,7 @@ func Warn(msg string, fields ...zap.Field) {
 
 // Warnf wraps Warnf of sugared logger, it uses fmt.Sprintf to log a templated message.
 func Warnf(template string, args ...interface{}) {
-	_sugaredGlobalLogger.Warnf(template, args)
+	_sugaredGlobalLogger.Warnf(template, args...)
 }
 
 // Error logs a message at ErrorLevel. The message includes any fields passed at the log site, as
@@ -72,7 +72,7 @@ func Error(msg string, fields ...zap.Field) {
 
 // Errorf wraps Errorf of sugared logger, it uses fmt.Sprintf to log a templated message.
 func Errorf(template string, args ...interface{}) {
-	_sugaredGlobalLogger.Errorf(template, args)
+	_sugaredGlobalLogger.Errorf(template, args...)
 }
 
 // Panic logs a message at PanicLevel. The message includes any fields passed at the log site, as
@@ -86,7 +86,7 @@ func Panic(msg string, fields ...zap.Field) {
 // Panicf wraps Panicf of sugared logger, it uses fmt.Sprintf to log a templated message. Other
 // behaviors are similar to Panic.
 func Panicf(template string, args ...interface{}) {
-	_sugaredGlobalLogger.Panicf(template, args)
+	_sugaredGlobalLogger.Panicf(template, args...)
 }
 
 // Fatal logs a message at FatalLevel. The message includes any fields passed at the log site, as
@@ -100,7 +100,7 @@ func Fatal(msg string, fields ...zap.Field) {
 // Fatalf wraps Fatalf of sugared logger, it uses fmt.Sprintf to log a templated message. Other
 // behaviors are similar to Fatal.
 func Fatalf(template string, args ...interface{}) {
-	_sugaredGlobalLogger.Fatalf(template, args)
+	_sugaredGlobalLogger.Fatalf(template, args...)
 }
 
 // Sync calls the underlying Core's Sync method, flushing any buffered log
