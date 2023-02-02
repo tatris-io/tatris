664092424e9ea1d401eeb30bede6c1ef41ce39cc
masaimu <masaimu@126.com>
2023-02-02 21:57:34 +0800

    use now to parse date

--- internal/core/index_test.go
+++ internal/core/index_test.go
@@ -4,7 +4,6 @@ package core
 
 import (
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -13,18 +12,12 @@ func TestDateParse(t *testing.T) {
 	dates := []string{
 		"2023-01-26T00:00:40Z",
 		"2023-01-26 08:00:40",
-		"2023-01-26T08:00:40",
 
 		"2023-01-26 08:00:40.000",
-		"2023-01-26T08:00:40.000",
 		"2023-01-26T00:00:40.000Z",
 	}
 	for _, date := range dates {
-		layout, err := detectTimeLayout(date)
-		assert.NoError(t, err)
-		t.Log(layout)
-
-		_, parseErr := time.Parse(layout, date)
-		assert.NoError(t, parseErr)
+		t.Log(date)
+		assert.True(t, isDateType(date))
 	}
 }
