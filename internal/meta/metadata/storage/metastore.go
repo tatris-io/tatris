cc7c345591357c7ac57ff2c0c9e996fa5b9afb2b
xiangwanpeng <xwanpeng@gmail.com>
2023-02-17 17:39:53 +0800

    style: add anno

--- internal/meta/metadata/storage/metastore.go
+++ internal/meta/metadata/storage/metastore.go
@@ -4,13 +4,17 @@
 package storage
 
 type MetaStore interface {
-	// Set Set("/x/y/z", bytes)
+	// Set sets the value for a key, bucket will be created if it does not exist
+	// example: Set("/x/y/z", bytes)
 	Set(string, []byte) error
-	// Get Get("/x/y/z")
+	// Get retrieves the value for a key
+	// example: Get("/x/y/z")
 	Get(string) ([]byte, error)
-	// List List("/x/y/")
+	// List returns all values under the bucket
+	// example: List("/x/y/")
 	List(string) (map[string][]byte, error)
-	// Delete Delete("/x/y/z")
+	// Delete removes a key from the bucket
+	// example: Delete("/x/y/z")
 	Delete(string) error
 	Close() error
 }
