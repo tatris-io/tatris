4433e1c35ba8c816031e9d42f0207234256b1ba5
xiangwanpeng <xwanpeng@gmail.com>
2023-02-24 14:31:34 +0800

    style: mark error time as 0

--- internal/query/query_doc.go
+++ internal/query/query_doc.go
@@ -129,28 +129,28 @@ func timeRange(query protocol.Query) (int64, int64, error) {
 			if timeRange.Gt != nil {
 				t, err := utils.ParseTime(timeRange.Gt)
 				if err != nil {
-					return start, end, err
+					return 0, 0, err
 				}
 				start = t.UnixMilli() + 1
 			}
 			if timeRange.Gte != nil {
 				t, err := utils.ParseTime(timeRange.Gte)
 				if err != nil {
-					return start, end, err
+					return 0, 0, err
 				}
 				start = t.UnixMilli()
 			}
 			if timeRange.Lt != nil {
 				t, err := utils.ParseTime(timeRange.Lt)
 				if err != nil {
-					return start, end, err
+					return 0, 0, err
 				}
 				end = t.UnixMilli() - 1
 			}
 			if timeRange.Lte != nil {
 				t, err := utils.ParseTime(timeRange.Lte)
 				if err != nil {
-					return start, end, err
+					return 0, 0, err
 				}
 				end = t.UnixMilli()
 			}
