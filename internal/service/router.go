e430e31572fed9b8f580fbd6864ebe46106aac7a
xiangwanpeng <xwanpeng@gmail.com>
2023-02-26 13:15:33 +0800

    feat: revise the protocol

--- internal/service/router.go
+++ internal/service/router.go
@@ -52,7 +52,7 @@ func StartHTTPServer(roles ...string) {
 		}
 	}
 
-	if err := router.Run(":8080"); err != nil {
+	if err := router.Run(":6060"); err != nil {
 		logger.Error(
 			"Tatris HTTP server start failed",
 			zap.Any("roles", roles),
@@ -64,6 +64,7 @@ func StartHTTPServer(roles ...string) {
 func registerIngestion(group *gin.RouterGroup) {
 	logger.Info("ingestion APIs registering")
 	group.PUT("/:index/_ingest", handler.IngestHandler)
+	group.POST("/:index/_ingest", handler.IngestHandler)
 }
 
 func registerQuery(group *gin.RouterGroup) {
@@ -75,15 +76,18 @@ func registerMeta(group *gin.RouterGroup) {
 	logger.Info("meta APIs registering")
 
 	group.PUT("/:index", handler.CreateIndexHandler)
+	group.POST("/:index", handler.CreateIndexHandler)
 	group.GET("/:index", handler.GetIndexHandler)
 	group.DELETE("/:index", handler.DeleteIndexHandler)
 	group.HEAD("/:index", handler.IndexExistHandler)
 
 	group.PUT("/_indices/:index", handler.CreateIndexHandler)
+	group.POST("/_indices/:index", handler.CreateIndexHandler)
 	group.GET("/_indices/:index", handler.GetIndexHandler)
 	group.DELETE("/_indices/:index", handler.DeleteIndexHandler)
 	group.HEAD("/_indices/:index", handler.IndexExistHandler)
 
+	group.PUT("/_aliases", handler.ManageAliasHandler)
 	group.POST("/_aliases", handler.ManageAliasHandler)
 	group.GET("/_aliases", handler.GetAliasHandler)
 	group.GET("/:index/_aliases", handler.GetAliasHandler)
@@ -91,6 +95,7 @@ func registerMeta(group *gin.RouterGroup) {
 	group.GET("/:index/_aliases/:alias", handler.GetAliasHandler)
 
 	group.PUT("/_index_template/:template", handler.CreateIndexTemplateHandler)
+	group.POST("/_index_template/:template", handler.CreateIndexTemplateHandler)
 	group.GET("/_index_template/:template", handler.GetIndexTemplateHandler)
 	group.DELETE("/_index_template/:template", handler.DeleteIndexTemplateHandler)
 	group.HEAD("/_index_template/:template", handler.IndexTemplateExistHandler)
